a8c4452cac174eb68ba22fcdb4c804d00c413f35
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-01-30 09:21:20 -0800

    feat: champion select grid

--- src/pages/champions/index.tsx
+++ src/pages/champions/index.tsx
@@ -1,7 +1,40 @@
-export default function Page() {
+import { lol_api } from "@/util/client";
+import { useRouter } from "next/router";
+import type { ChampionsDataDragonDetails } from "twisted/dist/models-dto";
+
+type Props = {
+	data: ChampionsDataDragonDetails;
+};
+
+export async function getServerSideProps() {
+	const data = await lol_api.DataDragon.getChampion();
+	return {
+		props: {
+			data: data.data,
+		},
+	};
+}
+
+export default function Page({ data }: Props) {
+	const router = useRouter();
 	return (
-		<div>
+		<>
 			<span>hello champions!</span>
-		</div>
+			<div className="grid grid-cols-4 gap-3 justify-items-center">
+				{Object.values(data).map((champion) => {
+					return (
+						<button
+							key={champion.id}
+							className="bg-blue-500 hover:bg-blue-700 text-white w-1/4 mt-3"
+							onClick={() => {
+								router.push(`/champions/${champion.id}`);
+							}}
+						>
+							{champion.name}
+						</button>
+					);
+				})}
+			</div>
+		</>
 	);
 }
